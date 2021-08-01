009f646a675d6dce8549f90865697c6365718c3b
pablo m <pablo_89_cba@hotmail.com>
2021-08-01 16:16:13 -0300

    Primer comit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '88a451b5464adecec953b731f22494af'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '50977518-f645-49a4-a944-1d5aae7ee7f3'
+};
